78fc202d17572aa9a7516327061964296691c64e
Yoshihide Chubachi <yoshi@chubachi.net>
2011-10-29 23:06:37 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZxTemplateEngine::Application.config.secret_token = '7c4b987656174f4cd7fb02f28563b6de4d2895dc607c4f18499828c6edf590b6c465adf6579c9a47e410df2d0c3e4c937288c5da3c2b2f8c2610aac46e94f816'
