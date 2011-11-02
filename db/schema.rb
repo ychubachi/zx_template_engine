dd8116ac089dad0d453b2159ce7fd1c8114408c8
Yoshihide Chubachi <yoshi@chubachi.net>
2011-11-02 19:46:31 +0900

    done

--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111030130658) do
-
-  create_table "comments", :force => true do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
-
-  create_table "posts", :force => true do |t|
-    t.string   "name"
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20111102103852) do
 
   create_table "templates", :force => true do |t|
     t.string   "filename"
@@ -37,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20111030130658) do
     t.string   "memo"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "test"
   end
 
 end
