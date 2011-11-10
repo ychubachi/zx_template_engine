9099e8b61f945871694dd891d16c283ca75b02e6
Yoshihide Chubachi <yoshi@chubachi.net>
2011-11-10 12:57:19 +0900

    Learned how to youse filds_for helper

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,4 +1,5 @@
 class Instance < ActiveRecord::Base
   belongs_to :template
   has_many :values
+  accepts_nested_attributes_for :values, :allow_destroy => true
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,4 +1,6 @@
 class Template < ActiveRecord::Base
   has_many :placeholders, {:dependent => :delete_all}
+  accepts_nested_attributes_for :placeholders, :allow_destroy => true
   has_many :instances, {:dependent => :delete_all}
+  accepts_nested_attributes_for :instances, :allow_destroy => true
 end
