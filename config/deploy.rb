48d85f0e763ba835f713ef8b3dbd401f180c1707
Yoshihide Chubachi <yoshi@chubachi.net>
2011-12-09 22:39:13 +0900

    Symlinks

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,20 +4,48 @@ set :deploy_to, "/home/rails/zx_template_engine"
 set :use_sudo, false
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "kirin.chubachi.net"                          # Your HTTP server, Apache/etc
-role :app, "kirin.chubachi.net"                          # This may be the same as your `Web` server
-role :db,  "kirin.chubachi.net", :primary => true # This is where Rails migrations will run
+role :web, "kirin.chubachi.net"
+role :app, "kirin.chubachi.net"
+role :db,  "kirin.chubachi.net", :primary => true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-#If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :deploy do
+  desc <<-DESC
+    Creates synlinks for this application.
+  DESC
+  task :mylinks, :roles => :app do
+    # DB
+    src = File.join(deploy_to, 'shared', 'db', 'production.sqlite3')
+    dst = File.join(current_path,'db')
+    run "ln -s #{src} #{dst}"
+    # Uploads
+    src = File.join(deploy_to, 'shared', 'uploads')
+    dst = File.join(current_path,'public')
+    run "ln -s #{src} #{dst}"
+    # Generated
+    src = File.join(deploy_to, 'shared', 'generated')
+    dst = File.join(current_path,'public')
+    run "ln -s #{src} #{dst}"
+  end
+
+  desc <<-DESC
+    Deploy everything. This will work similarly to the 'migrations' task, 
+    but will create the application symlinks.
+  DESC
+  task :all do
+    set :migrate_target, :latest
+    update_code
+    symlink
+    mylinks
+    migrate
+    restart
   end
 end
