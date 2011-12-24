386d4e733b4b7ee5f97e20661d0765f27d7f5ff9
Yoshihide Chubach (Luna CentOS) <yoshi@chubachi.net>
2011-12-24 15:26:39 +0900

    replaceのlogicを基にもどしました。

--- lib/zip_replacer.rb
+++ lib/zip_replacer.rb
@@ -64,11 +64,14 @@ class ZipReplacer
             File.open(file, 'r') do |input|
               input.each do |line|
                 replacements.each do |k,v|
-                  puts k
                   next if v == nil
                   placeholder = '#{' + k + '}'	# PLACEHOLDER
                   if ! line.scan(/#{placeholder}/).empty?
+                    puts 'found'
+                    puts 'v=' + v
+                    puts 'p=' + placeholder
                     line.gsub!(/#{placeholder}/, v)
+                    # puts line
                   end
                 end
                 output << line
@@ -88,7 +91,7 @@ class ZipReplacer
 
     return zip_file_path
   end
-  
+
   private
 
   def unzip(file_path)
@@ -133,3 +136,13 @@ class ZipReplacer
     return unzip_dir
   end
 end
+
+=begin
+      key = value.placeholder.key # </w:t></w:r><w:r w:rsidR="002F6123"><w:rPr><w:rFonts w:hint="eastAsia"/><w:szCs w:val="21"/></w:rPr><w:t>請求月</w:t></w:r><w:r w:rsidR="002F6123"><w:rPr><w:rFonts w:hint="eastAsia"/><w:szCs w:val="21"/></w:rPr><w:t>
+      logger.debug 'key = ' + key
+      striped_key = key.gsub(/<[^>]*>/ui,'') # "請求月"
+      text_value = value.value # "1"
+      value = key.gsub(/#{striped_key}/, text_value) # </w:t></w:r><w:r w:rsidR="002F6123"><w:rPr><w:rFonts w:hint="eastAsia"/><w:szCs w:val="21"/></w:rPr><w:t>1</w:t></w:r><w:r w:rsidR="002F6123"><w:rPr><w:rFonts w:hint="eastAsia"/><w:szCs w:val="21"/></w:rPr><w:t>
+      logger.debug 'key = ' + key
+      logger.debug 'value = ' + value
+=end
